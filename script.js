f077bc63e52b92b8921114bc1746eab7e6aa6f9a
Rakshit Diwani <rakshitdiwani@gmail.com>
2025-08-09 20:51:47 +0530

    100% done

--- script.js
+++ script.js
@@ -1,64 +1,78 @@
 console.log("started");
 
-const apiKey="866bcfd4de68b79405f18aba7110db91";
+const apiKey = "866bcfd4de68b79405f18aba7110db91";
 
-function getWeather(){
-    const city=document.getElementById("city-input").value;
-    if(!city){
-        alert("Please Enter a City Name");
-        return;
-    }
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function detailsFetching(url) {
     fetch(url)
         .then(response => response.json())
-        .then(data =>{
-            console.log(data)
-            if(data.cod === "404"){
+        .then(data => {
+            console.log(data);
+            if (data.cod === "404") {
                 window.location.href = "404.html";
-            }
-            else{
+            } else {
+                const city = data.name || "Unknown location";
                 const temp = parseInt(data.main.temp);
                 const description = data.weather[0].description;
                 const icon = data.weather[0].icon;
-                const minTemp= data.main.temp_min;
-                const maxTemp= data.main.temp_max;
+                const minTemp = data.main.temp_min;
+                const maxTemp = data.main.temp_max;
                 const feelsLike = data.main.feels_like;
                 const humidity = data.main.humidity;
                 const windSpeed = data.wind.speed;
                 const visibility = data.visibility / 1000;
                 const pressure = data.main.pressure;
-                const sunrise =data.sys.sunrise;
-                const sunset =data.sys.sunset;
+                const sunrise = data.sys.sunrise;
+                const sunset = data.sys.sunset;
                 const options = { hour: '2-digit', minute: '2-digit', hour12: true };
                 const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString('en-US', options);
                 const sunsetTime = new Date(sunset * 1000).toLocaleTimeString('en-US', options);
 
-
-                document.getElementById("city").innerHTML = city.charAt(0).toUpperCase() + city.substring(1);
+                document.getElementById("city").innerText = city.charAt(0).toUpperCase() + city.substring(1);
                 document.querySelector(".description").innerHTML = description.charAt(0).toUpperCase() + description.substring(1);
-                document.querySelector(".temp").innerHTML = temp +"°C";
+                document.querySelector(".temp").innerHTML = temp + "°C";
                 document.querySelector(".feels-like").innerHTML = `Feels like ${feelsLike} °C`;
-                document.getElementById("min-temp").innerHTML= minTemp;
-                document.getElementById("max-temp").innerHTML= maxTemp;
-                document.querySelector(".weather-icon").innerHTML =  `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" ></img>`;
+                document.getElementById("min-temp").innerText = minTemp;
+                document.getElementById("max-temp").innerText = maxTemp;
+                document.querySelector(".weather-icon").innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`;
 
-                document.getElementById("hum").innerHTML= humidity;
-                document.getElementById("pre").innerHTML= (pressure/1000).toFixed(3);
-                document.getElementById("sun-rise").innerHTML= sunriseTime;
-                document.getElementById("sun-set").innerHTML= sunsetTime;
-                document.getElementById("visi").innerHTML = visibility;
-                document.getElementById("wind").innerHTML= windSpeed;
-
-            
+                document.getElementById("hum").innerText = humidity;
+                document.getElementById("pre").innerText = (pressure / 1000).toFixed(3);
+                document.getElementById("sun-rise").innerText = sunriseTime;
+                document.getElementById("sun-set").innerText = sunsetTime;
+                document.getElementById("visi").innerText = visibility;
+                document.getElementById("wind").innerText = windSpeed;
             }
-
-        });
-    
+        })
+        .catch(err => console.error("Error fetching weather:", err));
 }
 
+function getWeather() {
+    const city = document.getElementById("city-input").value;
+    if (!city) {
+        alert("Please Enter a City Name");
+        return;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=en`;
+    detailsFetching(url);
+}
 
+console.log("Location fetching started");
+const locationButton = document.getElementById("getLocation");
 
+function gotLocation(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log(lat, lon);
+    const lurl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=en`;
+    detailsFetching(lurl);
+}
 
+function failedToGet() {
+    console.log("There was some issue in fetching location");
+}
 
+locationButton.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
+});
 
-console.log("script.js loaded");    
+console.log("script.js loaded");
